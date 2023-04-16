d1ab73df8336979ff24e1e64c2019bee42a4e004
Gláufer Charão <glaufer.charao@gmail.com>
2023-04-16 00:19:18 -0300

    Inserindo nova Categoria.

--- src/main/java/com/gsamtecnologia/curso_ionic/resources/CategoriaResource.java
+++ src/main/java/com/gsamtecnologia/curso_ionic/resources/CategoriaResource.java
@@ -4,11 +4,10 @@ import com.gsamtecnologia.curso_ionic.dto.CategoriaDTO;
 import com.gsamtecnologia.curso_ionic.services.CategoriaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,5 +24,13 @@ public class CategoriaResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<CategoriaDTO> buscarPorId(@PathVariable Long id){
         return ResponseEntity.ok().body(service.buscarPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoriaDTO> salvar(@RequestBody CategoriaDTO categoriaDTO){
+        categoriaDTO = service.salvar(categoriaDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(categoriaDTO.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
